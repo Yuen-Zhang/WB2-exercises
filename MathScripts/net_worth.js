f9da690ed7bb3719ba1be78cea13ab947a140ef1
Yuen-Zhang <yuen.en@hotmail.com>
2023-04-25 13:17:26 -0700

    1st commit
    net worth complete

---
+++ MathScripts/net_worth.js
@@ -0,0 +1,10 @@
+"use strict"
+//set assets and debts
+let myAssets = 300000;
+let myDebts = 10000;
+
+//calculation of net worth
+let netWorth = myAssets - myDebts;
+
+//print out the net worth
+console.log('Your net worth is ' + netWorth);
