8cf038d11c20b12cc23b0528c9124029f7e23176
Yuen-Zhang <yuen.en@hotmail.com>
2023-04-26 11:23:36 -0700

    4th commit
    finish module 2

---
+++ NumericConversions/conversion_tests.js
@@ -0,0 +1,24 @@
+// Description: This script tests various numeric
+// conversion techniques
+// Author: Jordan Q. Newprogrammer
+let a = " 101.1 ";
+let b = "55";
+let c = "402 Stevens";
+let d = "Number 5 ";
+//parseInt()
+//parseFloat()
+//Number()
+//+
+a = +a;
+b = +b;
+c = +c;
+d = +d;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+// parseInt() 101 55 402 NaN
+//parseFloat() 101.1 55 402 NaN
+//Number() 101.1 55 NaN NaN
+//+ 101.1 55 NaN NaN
+
